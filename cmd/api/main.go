db156e977bfbd4c975392a432b592f97947f0174
Roman Sharkov <roman.sharkov@qbeon.com>
2019-05-01 16:43:44 +0200

    Remove obsolete code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"flag"
 	"log"
-	"time"
 
 	"github.com/romshark/dgraph_graphql_go/api"
 	"github.com/romshark/dgraph_graphql_go/store"
-	"github.com/romshark/dgraph_graphql_go/store/dbmod"
 )
 
 var host = flag.String("host", "localhost:16000", "API server host address")
@@ -35,29 +33,6 @@ func main() {
 		if err := api.Shutdown(context.Background()); err != nil {
 			log.Fatalf("API server shutdown: %s", err)
 		}
-	})
-
-	time.AfterFunc(time.Millisecond*50, func() {
-		ctx := context.Background()
-		aliceID, err := str.CreateUser(ctx, "alice@robinsons.net", "alice")
-		if err != nil {
-			log.Fatalf("alice creation: %s", err)
-		}
-
-		var aliceRes struct {
-			Alice []dbmod.User `json:"alice"`
-		}
-		str.QueryVars(ctx, `
-			query Alice($id: string) {
-				alice(func: eq(User.id, $id)) {
-					uid
-					User.displayName
-					User.email
-				}
-			}
-		`, map[string]string{
-			"$id": string(aliceID),
-		}, &aliceRes)
 	})
 
 	api.AwaitShutdown()
