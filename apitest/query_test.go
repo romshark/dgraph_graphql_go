f40d4d9baad60f7fd54c6656fb6a4c847d21a737
Roman Sharkov <roman.sharkov@qbeon.com>
2019-05-01 03:17:00 +0200

    Add API test

--- apitest/query_test.go
+++ apitest/query_test.go
@@ -12,19 +12,21 @@ import (
 // TestQuery tests post creation
 func TestQuery(t *testing.T) {
 	type TestSetup struct {
-		ts    *setup.TestSetup
-		users map[store.ID]*gqlmod.User
-		posts map[store.ID]*gqlmod.Post
+		ts            *setup.TestSetup
+		users         map[store.ID]*gqlmod.User
+		posts         map[store.ID]*gqlmod.Post
+		postsByAuthor map[store.ID]map[store.ID]*gqlmod.Post
 	}
 
 	setupTest := func(t *testing.T) TestSetup {
 		ts := setup.New(t, tcx)
 
 		userA := ts.Help.OK.CreateUser("fooBarowich", "foo@bar.buz")
+		userB := ts.Help.OK.CreateUser("buzBazowich", "buz@foo.foo")
+		userC := ts.Help.OK.CreateUser("fuzFuzzowich", "fuz@fuz.fuz")
+
 		postA1 := ts.Help.OK.CreatePost(*userA.ID, "A post 1", "test content 1")
 		postA2 := ts.Help.OK.CreatePost(*userA.ID, "A post 2", "test content 2")
-
-		userB := ts.Help.OK.CreateUser("buzBazowich", "buz@foo.foo")
 		postB1 := ts.Help.OK.CreatePost(*userB.ID, "B post 1", "test content 3")
 
 		users := make(map[store.ID]*gqlmod.User, 2)
@@ -36,10 +38,26 @@ func TestQuery(t *testing.T) {
 		posts[*postA2.ID] = postA2
 		posts[*postB1.ID] = postB1
 
+		// Index: posts by author
+		postsByAuthor := make(
+			map[store.ID]map[store.ID]*gqlmod.Post,
+			len(posts),
+		)
+
+		postsByAuthor[*userA.ID] = make(map[store.ID]*gqlmod.Post, 2)
+		postsByAuthor[*userA.ID][*postA1.ID] = postA1
+		postsByAuthor[*userA.ID][*postA2.ID] = postA2
+
+		postsByAuthor[*userB.ID] = make(map[store.ID]*gqlmod.Post, 1)
+		postsByAuthor[*userB.ID][*postB1.ID] = postB1
+
+		postsByAuthor[*userC.ID] = make(map[store.ID]*gqlmod.Post)
+
 		return TestSetup{
-			ts:    ts,
-			users: users,
-			posts: posts,
+			ts:            ts,
+			users:         users,
+			posts:         posts,
+			postsByAuthor: postsByAuthor,
 		}
 	}
 
@@ -156,6 +174,45 @@ func TestQuery(t *testing.T) {
 			)
 			require.NotNil(t, query.Post)
 			require.Equal(t, *expected, *query.Post)
+		}
+	})
+
+	t.Run("user.posts", func(t *testing.T) {
+		s := setupTest(t)
+		defer s.ts.Teardown()
+
+		for authorID, posts := range s.postsByAuthor {
+			var query struct {
+				User *gqlmod.User `json:"user"`
+			}
+			s.ts.QueryVar(
+				`query($userId: Identifier!) {
+					user(id: $userId) {
+						posts {
+							id
+							title
+							contents
+							creation
+							author {
+								id
+							}
+						}
+					}
+				}`,
+				map[string]string{
+					"userId": string(authorID),
+				},
+				&query,
+			)
+
+			require.NotNil(t, query.User)
+			require.Len(t, query.User.Posts, len(posts))
+
+			for _, actualPost := range query.User.Posts {
+				id := *actualPost.ID
+				require.Contains(t, posts, id)
+				require.Equal(t, *posts[id], actualPost)
+			}
 		}
 	})
 }
