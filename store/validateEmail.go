f39f70692220909bca9d852d2ef7f282b7414a38
Roman Sharkov <roman.sharkov@qbeon.com>
2019-05-01 14:30:45 +0200

    Fix email validation
    Make the email validator check maximum length.

--- store/validateEmail.go
+++ store/validateEmail.go
@@ -18,8 +18,11 @@ func init() {
 
 // ValidateEmail returns an error if invalid, otherwise returns nil
 func ValidateEmail(v string) error {
+	if len(v) > 96 {
+		return errors.Errorf("email address too long (%d)", len(v))
+	}
 	if !regexpEmail.MatchString(v) {
-		return errors.Errorf("invalid email")
+		return errors.Errorf("invalid email address")
 	}
 	return nil
 }
