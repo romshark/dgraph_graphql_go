cdf4a33145ce2960857ba4a1c41a1ef1aae10e80
Roman Sharkov <roman.sharkov@qbeon.com>
2019-05-04 16:38:09 +0200

    Fix cmd/api main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/romshark/dgraph_graphql_go/api"
 	"github.com/romshark/dgraph_graphql_go/api/passhash"
 	"github.com/romshark/dgraph_graphql_go/api/sesskeygen"
-	"github.com/romshark/dgraph_graphql_go/store"
 )
 
 var host = flag.String("host", "localhost:16000", "API server host address")
@@ -17,17 +16,12 @@ var dbHost = flag.String("dbhost", "localhost:9080", "database host address")
 func main() {
 	flag.Parse()
 
-	str := store.NewStore()
-	if err := str.Prepare(); err != nil {
-		log.Fatalf("store prepare: %s", err)
-	}
-
 	api := api.NewServer(api.ServerOptions{
 		Host:                *host,
 		DBHost:              *dbHost,                 // database host address
 		SessionKeyGenerator: sesskeygen.NewDefault(), // session key generator
 		PasswordHasher:      passhash.Bcrypt{},       // password hasher
-	}, str)
+	})
 
 	if err := api.Launch(); err != nil {
 		log.Fatalf("API server launch: %s", err)
